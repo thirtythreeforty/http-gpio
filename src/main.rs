24a8e68d2ed983d77d599cf29005d82c153b9ca2
George Hilliard <thirtythreeforty@gmail.com>
2020-05-02 18:28:18 -0500

    Do initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,57 @@
-use warp::Filter;
+use gpio_cdev::{Chip, LineRequestFlags};
+use serde::{Serialize, Deserialize};
+use warp::{Filter, http::StatusCode};
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let gpio_hello = warp::path!("gpio")
+        .map(|| "This is the GPIO API");
 
-    warp::serve(hello)
+    let gpio_modify = warp::post()
+        .and(warp::path!("gpio" / String / u32))
+        /* 1KB should be enough for anyone */
+        .and(warp::body::content_length_limit(1024))
+        .and(warp::body::json())
+        .map(gpio_modify)
+        .map(as_reply);
+
+    let routes = gpio_hello.or(gpio_modify);
+
+    warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
+}
+
+#[derive(Serialize,Deserialize,Debug)]
+enum GpioCmd {
+    In,
+    Out {
+        value: bool,
+    },
+}
+
+type GpioModifyResult = Result<(), gpio_cdev::errors::Error>;
+
+fn gpio_modify(chip: String, pin: u32, body: GpioCmd) -> GpioModifyResult {
+    let line = Chip::new(format!("/dev/{}", chip))?.get_line(pin)?;
+    match body {
+        GpioCmd::Out { value } => {
+            line.request(LineRequestFlags::OUTPUT, 0, "http-gpio")?
+                .set_value(value as u8)
+        }
+        GpioCmd::In => {
+            line.request(LineRequestFlags::INPUT, 0, "http-gpio")?;
+            Ok(())
+        }
+    }
+}
+
+fn as_reply(value: GpioModifyResult) -> Box<dyn warp::Reply> {
+    // Return if success, or stringify the error if not
+    match value {
+        Ok(_) => Box::new("Success"),
+        Err(err) => Box::new(
+            warp::reply::with_status(err.to_string(),
+                                     StatusCode::INTERNAL_SERVER_ERROR))
+    }
 }
